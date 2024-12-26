4e6b6add3250db5d87afa0e8b8c07e2a1a6d4b94
Thorsten Staerk <thorsten@staerk.de>
2024-12-26 16:35:30 +0100

    Create marcopolo.py
    This is just a chatbot for Discord that will listen for "Marco" and reply "Polo". Made as a start and pipe cleaner :)

---
+++ marcopolo.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+
+# Set up the bot
+intents = discord.Intents.all()
+intents.messages = True  # Enable message intent
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Event: Bot is ready
+@bot.event
+async def on_ready():
+    print(f"We have logged in as {bot.user}")
+
+# Event: Respond to "Marco"
+@bot.event
+async def on_message(message):
+    print("saw something")
+    print(message)
+    print("Content: ")
+    print(message.content.lower())
+
+    # Ignore messages from the bot itself
+    if message.author == bot.user:
+        return
+    # Check for "Marco" and reply "Polo"
+    if message.content.lower() == "marco":
+        await message.channel.send("Polo")
+
+    # Allow other commands to process
+    await bot.process_commands(message)
+
+# Run the bot
+bot.run("replace with your channel ID, for me, this has 73 characters")
